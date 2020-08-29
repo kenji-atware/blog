a5b028428c17c43c054dfd0df6fb0f74efef53c5
kenji-atware <ngotoanminhtue@gmail.com>
2020-08-29 16:18:52 +0700

    remove origami

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,10 +19,6 @@ module.exports = {
             title: `About`,
             slug: `/about`,
           },
-          {
-            title: `Origami`,
-            slug: `/origami`,
-          }
         ],
         externalLinks: [
           {
@@ -92,7 +88,7 @@ module.exports = {
     // Used for SEO
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     // Will be set on the <html /> tag
-    siteLanguage: `vi`,
+    siteLanguage: `en`,
     // Used for og:image and must be placed inside the `static` folder
     siteImage: `/banner.jpg`,
     // Twitter Handle
